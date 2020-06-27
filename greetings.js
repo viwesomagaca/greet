a6bee7ab5c2b16456c67205661c7913299c7659b
Viwe <viwesomagaca29@gmail.com>
2020-06-27 22:56:59 +0200

    fixed errors and changed background

--- greetings.js
+++ greetings.js
@@ -1,9 +1,13 @@
 var countbtn = document.querySelector('#count');
 var num = localStorage.getItem('currentNumberOfGreetings');
-document.getElementById('count').innerHTML = 'Greets: ' + num;
+document.getElementById('count').innerHTML = 'Greetings: ' + num;
 
 var namesGreeted = {};
 for (var i=0; i< namesGreeted.length; i++){
+}
+
+if( !num){
+    countbtn.innerHTML = 'Greetings: ' + 0;
 }
 
 function displayInput() {
@@ -14,25 +18,25 @@ function displayInput() {
         namesGreeted[x]=1;
         /*>-- incremate my counter by one*/
         num++;
-        /*>-- and display the incremetend number to my page*/
-        countbtn.innerHTML = 'Greets: ' + num;
+        /*>-- and display the incremeted number to my page*/
+        countbtn.innerHTML = 'Greetings: ' + num;
         /*Than display this greeting*/
         document.getElementById('displayHere').innerHTML = "Good Day, " + document.getElementById('elements').value;
         
     } else if (document.getElementById('Isixhosa').checked === true && x.length > 0 && x !== " " && namesGreeted[x] === undefined) {
         namesGreeted[x]=1;
         num++;
-        countbtn.innerHTML = 'Greets: ' + num;
+        countbtn.innerHTML = 'Greetings: ' + num;
         document.getElementById('displayHere').innerHTML = "Molo, " + document.getElementById('elements').value;
     } else if (document.getElementById('Afrikaans').checked === true && x.length > 0 && x !== " " && namesGreeted[x] === undefined) {
         namesGreeted[x]=1;
         num++;
-        countbtn.innerHTML = 'Greets: ' + num;
+        countbtn.innerHTML = 'Greetings: ' + num;
         document.getElementById('displayHere').innerHTML = "Goeie Dag, " + document.getElementById('elements').value;
     }
     /*after the greet me button is pressed the text box will clear*/
     document.getElementById('elements').value = "";
-    /*the locStor.setItem stores the cyrrent Number of Greetings*/
+    /*the locStor.setItem stores the cyrrent Greetings*/
     localStorage.setItem('currentNumberOfGreetings', num);
     //console.log(localStorage.getItem('currentNumberOfGreetings'))
 }
@@ -42,7 +46,7 @@ f.addEventListener('click', dltcounter);
 
 function dltcounter() {
     'use strict';
-    countbtn.innerHTML = 'Greets: ' + num;
+    countbtn.innerHTML = 'Greetings: ' + num;
     localStorage.clear();
     num = 0;
 }
